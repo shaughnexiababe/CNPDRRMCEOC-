de2866f523a5365ce29060c3f2c17dcd22c50743
Awesomesauce0626 <i.am.sam052408@gmail.com>
2026-04-26 00:34:37 +0800

    Fix: Resolve React Router v7 future flag warnings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,7 +63,7 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
+        <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <AuthenticatedApp />
         </Router>
         <Toaster />
